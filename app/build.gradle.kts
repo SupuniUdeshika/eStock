9197ed57316e0ad2827fa2743522e0d421fd2a85
ASUS <hmsupuniudeshika@gmail.>
2025-09-04 21:57:57 +0530

    Starting Estock by adding

---
+++ app/build.gradle.kts
@@ -0,0 +1,54 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.google.gms.google.services)
+}
+
+android {
+    namespace = "com.example.estock"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.estock"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    implementation(libs.firebase.storage)
+    implementation(libs.firebase.firestore)
+    implementation(libs.firebase.auth)
+    implementation(libs.credentials)
+    implementation(libs.credentials.play.services.auth)
+    implementation(libs.googleid)
+    implementation("com.jakewharton.timber:timber:5.0.1")
+    implementation ("org.apache.poi:poi:5.2.3")
+    implementation ("org.apache.poi:poi-ooxml:5.2.3")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
---
+++ app/src/main/java/com/example/estock/Home.java
@@ -0,0 +1,71 @@
+package com.example.estock;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.protobuf.Option;
+
+public class Home extends AppCompatActivity {
+
+    LinearLayout ItemView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_home);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.home), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        ItemView = findViewById(R.id.ItemView);
+
+        // Set a click listener for the LinearLayout
+        ItemView.setOnClickListener(v -> {
+            Intent intent = new Intent(Home.this, Option.class);
+            startActivity(intent);
+        });
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
+
+        // Set the currently selected item
+        bottomNavigationView.setSelectedItemId(R.id.home_btn);
+
+        // Set the listener for item selection
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.home_btn) {
+                // Stay on the current activity
+                return true;
+            } else if (itemId == R.id.add_btn) {
+                // Navigate to the Module activity
+                startActivity(new Intent(getApplicationContext(), Option.class));
+                overridePendingTransition(R.anim.slide_right, R.anim.slide_left); // Apply transition animation
+                finish(); // Close the current activity
+                return true;
+            }
+
+            else if (itemId == R.id.user_btn) {
+                // Navigate to the Module activity
+                startActivity(new Intent(getApplicationContext(), User.class));
+                overridePendingTransition(R.anim.slide_right, R.anim.slide_left); // Apply transition animation
+                finish(); // Close the current activity
+                return true;
+            }
+
+            return false; // Unhandled cases
+        });
+    }
+}
